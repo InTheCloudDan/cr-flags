1e6a0fe13c497f74748d06d758017b3ce87b419c
Dan O'Brien <dobrien@launchdarkly.com>
2021-02-13 11:55:55 -0500

    test getting diff

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,6 +20,19 @@ func main() {
 		fmt.Printf("error parsing GitHub event payload at %q: %v", os.Getenv("GITHUB_EVENT_PATH"), err)
 	}
 	fmt.Println(event)
+	client := github.Client{}
+	ctx := context.Background()
+	owner := os.Getenv("GITHUB_REPOSITORY_OWNER")
+	repo := os.Getenv("GITHUB_REPO")
+
+	prService := github.PullRequestsService(*client.PullRequests)
+	rawOpts := github.RawOptions{Type: github.Diff}
+	raw, gh, err := prService.GetRaw(ctx, owner, repo, *event.PullRequest.Number, rawOpts)
+	fmt.Println(gh)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(raw)
 }
 
 func parseEvent(path string) (*github.PullRequestEvent, error) {
