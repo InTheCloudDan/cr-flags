dfedf0c79d62712308ae8bd42fa4b3ad8080c04d
Dan O'Brien <dobrien@launchdarkly.com>
2021-04-11 16:12:05 -0400

    template fixes

--- main.go
+++ main.go
@@ -372,9 +372,7 @@ Default variation: ` + "`" + `{{(index .Flag.Variations .Environment.Fallthrough
 Off variation: ` + "`" + `{{(index .Flag.Variations .Environment.OffVariation).Value}}` + "`" + `
 Kind: **{{ .Flag.Kind }}**
 Temporary: **{{ .Flag.Temporary }}**
-{{- if not len(.Aliases) 0}}
-Aliases: {{range $alias := .Aliases }}` + "`" + `{{$alias}}` + "`" + `{{end}}
-{{- end}}
+
 `
 	tmpl := template.Must(template.New("comment").Funcs(template.FuncMap{"trim": strings.TrimSpace}).Parse(tmplSetup))
 	// if err != nil {
